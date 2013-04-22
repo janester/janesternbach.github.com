df0bc04794216ba5a48e085b329d8022342d5a0c
Jane Sternbach <janesternbach@gmail.com>
2013-04-22 03:00:16 -0400

    some scroll stuff

---
+++ port.js
@@ -0,0 +1,14 @@
+$(function(){
+  $("#plist").hide();
+  $("#p_btn").click(show_list);
+
+});
+
+function show_list(e) {
+  var height = $('#page2').offset().top;
+  e.preventDefault();
+  $('body').animate({scrollTop: height}, 'slow');
+  $("#plist").fadeIn(700);
+  $(".slide2").effect("slide",{ direction: "right" }, 700);
+  $(".slide1").effect("slide",700);
+}

6f9f8beabccc228d2aa96581c08f17275b5ac28a
Jane Sternbach <janesternbach@gmail.com>
2013-04-22 20:23:32 -0400

    nav bar

--- port.js
+++ port.js
@@ -1,14 +1,53 @@
 $(function(){
   $("#p_btn").click(show_list);
-
+  navHeight = $('#nav-bar').offset().top;
+  $(window).scroll(function(){
+    if($(window).scrollTop() >= navHeight)
+      {
+      // sticky nav
+      $("#nav-bar-holder-fixed").addClass("nav");
+      var p = $("#p_btn").detach();
+      var a = $("#a_btn").detach();
+      var c = $("#c_btn").detach();
+      $("#nav-bar-holder-fixed").append(p).append(a).append(c);
+      }
+    if($(window).scrollTop() < navHeight)
+    {
+      $("#nav-bar-holder-fixed").removeClass("nav");
+      var p2 = $("#p_btn").detach();
+      var a2 = $("#a_btn").detach();
+      var c2 = $("#c_btn").detach();
+      $("#nav-bar").append(p2).append(a2).append(c2);
+    }
+  });
 });
 
+
 function show_list(e) {
-  $("#plist").hide();
-  var height = $('#page2').offset().top;
+  var height = $('#first_p').offset().top;
   e.preventDefault();
-  $('body').animate({scrollTop: height}, 'slow');
+  $('body').animate({scrollTop: height-60}, 'slow');
   $("#plist").fadeIn(700);
   $(".slide2").effect("slide",{ direction: "right" }, 700);
   $(".slide1").effect("slide",700);
 }
+
+
+
+
+
+
+
+//     }
+
+//     if($(window).scrollTop() < scrollTop){
+//       $('a#logo-link').fadeOut('slow', function() {
+//         // Animation complete
+//       });
+//       $('nav').removeAttr('style');
+
+//       $('#navigation-bar').removeAttr('style');
+
+//     }
+//   })
+// })
